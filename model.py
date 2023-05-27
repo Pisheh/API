57b0fd433f07dc658aaa2a47671cc9c3e236a545
bsimjoo <bsimjoo@gmail.com>
2023-05-27 13:03:08 +0330

    Changed data structure models

--- model.py
+++ model.py
@@ -2,45 +2,79 @@ from mongoengine import *
 from werkzeug.security import generate_password_hash, check_password_hash
 from enum import Enum
 
-    
-class Answer(Document):
+
+class Answer(EmbeddedDocument):
     content = StringField()
     score = IntField()
-    
-class Question(Document):
+
+
+class Question(EmbeddedDocument):
     question = StringField()
     answers = EmbeddedDocumentListField(Answer)
-    
+
+
 class Exam(Document):
     id = ObjectIdField(primary_key=True)
-    url = URLField()
     questions = EmbeddedDocumentListField(Question)
     questions_count = IntField()
-    
+
+
 class Skill(Document):
     id = ObjectIdField(primary_key=True)
     title = StringField()
     desc = StringField()
-    exam = ReferenceField(Exam)     # many to one
+    exams = ListField(ReferenceField(Exam))  # Many to Many
 
-class Seeker(Document):
+
+class Role(Enum):
+    SEEKER = 1
+    EMPLOYER = 2
+
+
+class User(Document):
     id = ObjectIdField(primary_key=True)
-    email = EmailField()
+    email = EmailField(index=True, unique=True)
     password_hash = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone_number = StringField(r'09\d{9}')
-    address = StringField()
-    cv = EmbeddedDocumentField("CV")
-    
-    
+    role = EnumField(Role)
+    extra_info = GenericLazyReferenceField()
+
     def verify_password(self, password):
         check_password_hash(self.password_hash, password)
-        
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-class CV(Document):
+
+class EmployerInfo(Document):
+    id = ObjectIdField(primary_key=True)
+    firstname = StringField()
+    lastname = StringField()
+    co_name = StringField()
+    co_address = StringField()
+    co_phones = ListField(StringField())
+    co_ver_code = StringField()
+    jobs = ListField(LazyReferenceField())
+
+
+class SeekerInfo(Document):
+    id = ObjectIdField(primary_key=True)
+    firstname = StringField()
+    lastname = StringField()
+    phone_number = StringField(r"09\d{9}")
+    cv = EmbeddedDocumentField("CV")
+
+
+class CV(EmbeddedDocument):
     content = StringField()
     skills = ListField(ReferenceField(Skill))
     scores = ListField(IntField)
+
+
+class job(Document):
+    id = ObjectIdField(primary_key=True)
+    title = StringField()
+    content = StringField()
+    skills = ListField(LazyReferenceField(Skill))
+    min_salary = IntField()
+    max_salary = IntField()
+    expire = DateTimeField()
