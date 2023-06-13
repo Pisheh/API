932f413b0a011f7da2ac0952b3e26e7a50826763
bsimjoo <bsimjoo@gmail.com>
2023-06-13 18:33:01 +0330

    implemented new database model

--- dbmodel.py
+++ dbmodel.py
@@ -5,12 +5,15 @@ from enum import Enum
 from typing import Any, Sequence
 from hashlib import md5
 from peewee import callable_
-from uuid import uuid1
 import datetime
 from datetime import timedelta
 import pydantic
 from schemas import SkillSchema, JobSchema, EmployerSchema
 from typing import Literal
+from slugify import slugify
+import uuid
+import shortuuid
+import json
 
 generate_password_hash = lambda p: pbkdf2_sha256.using(rounds=8000, salt_size=10).hash(
     p
@@ -38,15 +41,35 @@ _clone_set = lambda s: set(s) if s else set()
 _default = lambda arg, value: value if arg is DEFAULT else arg
 
 
+class JsonField(CharField):
+    def db_value(self, value):
+        return json.dumps(value)
+
+    def python_value(self, value):
+        return json.loads(value)
+
+
+class EnumField(IntegerField):
+    def __init__(self, choices: Enum, *args, **kwargs):
+        super(IntegerField, self).__init__(*args, **kwargs)
+        self.choices: Enum = choices
+
+    def db_value(self, value):
+        return value.value
+
+    def python_value(self, value):
+        return self.choices(value)
+
+
 class BaseModel(Model):
-    _schema_type_: pydantic.main.ModelMetaclass = None
+    _default_schema_: pydantic.main.ModelMetaclass = None
 
     class Meta:
         database = database_proxy
 
     def to_schema(self, type_: pydantic.BaseModel = DEFAULT) -> pydantic.BaseModel:
         data = {}
-        type_ = _default(type_, self._schema_type_)
+        type_ = _default(type_, self._default_schema_)
         for name, field in type_.__fields__.items():
             value = getattr(self, name)
             value_type = type(value)
@@ -61,57 +84,81 @@ class BaseModel(Model):
         return type_(**data)
 
 
+TABLES = []
+
+
+def add_table(model: Model):
+    TABLES.append(model)
+    for _, m2m in model._meta.manytomany.items():
+        m2m: ManyToManyField
+        TABLES.append(m2m.get_through_model())
+    return model
+
+
+@add_table
+class Guide(BaseModel):
+    slug = FixedCharField(64, primary_key=True)
+    title = CharField()
+    summary = CharField(null=True)
+    basic = TextField()
+    advanced = TextField(null=True)
+    branch = FixedCharField(64, index=True)
+    expertise = FixedCharField(64, index=True)
+
+    # personalities <> Personality.guides
+    # job_category - JobCategory.guide
+    # skills <> Skill.guides
+
+
+@add_table
 class Skill(BaseModel):
-    _backrefs_ = False  # exclude exams, seekers and jobs
-    _schema_type_ = SkillSchema
+    _default_schema_ = SkillSchema
 
+    slug = FixedCharField(64, primary_key=True)
     title = CharField()
-    desc = CharField(null=True)
-    # exams
-    # seekers
-    # jobs
+    description = CharField(null=True)
+    guides = ManyToManyField(Guide, backref="guides")
+    exam_scores = JsonField()
+
+    # jobs <> Job.skills
+    # exams < Exam
+    # seekers < SeekerSkill.skill
+    # courses < Course.skill
 
 
-class Exam(BaseModel):
-    _recurse_ = True
-    _backrefs_ = True
-    _exclude_ = ["skill"]
-
+@add_table
+class Course(BaseModel):
+    slug = FixedCharField(64, primary_key=True)
     title = CharField()
-    skill = ForeignKeyField(Skill, backref="exams")
-    # questions
+    description = CharField()
+    skill = ForeignKeyField(Skill, backref="courses")
+    link = CharField()
+    clicks = IntegerField()
 
 
-class Question(BaseModel):
-    _recurse_ = False
-    _backrefs_ = True
+@add_table
+class JobCategory(BaseModel):
+    slug = FixedCharField(64, primary_key=True)
+    title = CharField()
+    total_min_salary = IntegerField()
+    total_max_salary = IntegerField()
+    count_salary = IntegerField()
+    guide = ForeignKeyField(Guide, backref="job_category")
 
-    exam = ForeignKeyField(Exam, backref="questions")
-    content = CharField()
-    # answers
+    # personalities <> Personality.job_cats
+    # jobs < Job.category
 
+    @property
+    def avg_min_salary(self):
+        return self.total_min_salary / self.count_salary
 
-class Answer(BaseModel):
-    _recurse_ = False
-    _exclude_ = ["score"]
-
-    question = ForeignKeyField(Question, backref="answers")
-    content = CharField()
-    score = FloatField(default=0)
+    @property
+    def avg_mav_salary(self):
+        return self.total_max_salary / self.count_salary
 
 
 class User(BaseModel):
-    _exclude_ = [
-        "id",
-        "pass_hash",
-        "job_requests",
-        "phone_number",
-        "email",
-        "co_address",
-        "co_phones",
-        "co_ver_code",
-    ]
-    uuid = FixedCharField(36, index=True, default=uuid1, unique=True)
+    id = FixedCharField(22, default=shortuuid.uuid, primary_key=True)
     avatar = CharField(null=True)
     email = FixedCharField(64, index=True)
     phone_number = FixedCharField(9, index=True)
@@ -129,47 +176,103 @@ class User(BaseModel):
         return generate_password_hash(password)
 
 
+@add_table
 class Employer(User):
-    _schema_type_ = EmployerSchema
+    _default_schema_ = EmployerSchema
     co_name = TextField(null=True)
     co_address = TextField(null=True)
-    co_phones = TextField(null=True)
+    co_phones = JsonField(null=True)
     co_ver_code = TextField(null=True)
-    city = FixedCharField("30")
+    city = FixedCharField(30)
     # jobs
 
 
+@add_table
 class Seeker(User):
     firstname = CharField(null=True)
     lastname = CharField(null=True)
     cv_content = TextField(null=True)
-    # skills
+
+    # exam_results < ExamResult.seeker
+    # job_requests < JobRequest.seeker
+    # personalities <> Personality.users
+    # skills < SeekerSkill.seeker
 
 
-class SkillSeeker(BaseModel):
-    seeker = ForeignKeyField(Seeker, backref="skills")
+@add_table
+class Personality(BaseModel):
+    slug = FixedCharField(32, primary_key=True)
+    name = FixedCharField(32)
+    description = CharField()
+    users = ManyToManyField(Seeker, backref="personalities")
+    job_cats = ManyToManyField(JobCategory, backref="personalities")
+    guides = ManyToManyField(Guide, backref="personalities")
+
+    # exams < Exam.personality
+
+
+@add_table
+class SeekerSkill(BaseModel):
     skill = ForeignKeyField(Skill, backref="seekers")
+    seeker = ForeignKeyField(Seeker, backref="skills")
+    score = IntegerField()
+
+    # exam_results < ExamResult.seeker_skill
+
+
+@add_table
+class Exam(BaseModel):
+    class Types(Enum):
+        SKILL = 0
+        PERSONALITY = 1
+
+    title = CharField()
+    type = EnumField(Types)
+    skill = ForeignKeyField(Skill, backref="exams")
+    personality = ForeignKeyField(Personality, backref="exams")
+
+    # questions < Question.exam
+
+
+@add_table
+class Question(BaseModel):
+    exam = ForeignKeyField(Exam, backref="questions")
+    content = CharField()
+
+    # answers < Answer.question
+
+
+@add_table
+class Answer(BaseModel):
+    question = ForeignKeyField(Question, backref="answers")
+    content = CharField()
+    score = FloatField()
+
+
+@add_table
+class ExamResult(BaseModel):
+    exam = ForeignKeyField(Exam)
+    seeker = ForeignKeyField(Seeker, backref="exam_results")
+    seeker_skill = ForeignKeyField(SeekerSkill, backref="exam_results", null=True)
+    data = JsonField()
     score = IntegerField()
 
 
+@add_table
 class Job(BaseModel):
-    _exclude_ = ["requests", "expire_on", "expired", "requests"]
-    _max_depth_ = 1
     title = CharField()
     description = TextField()
-    requirements = CharField()
+    requirements = JsonField()
+    skills = ManyToManyField(Skill, backref="jobs")
+    category = ForeignKeyField(JobCategory, backref="jobs")
     min_salary = IntegerField()
     max_salary = IntegerField()
     created_on = DateTimeField(default=datetime.datetime.now)
     expire_on = DateTimeField()
     expired = BooleanField(False, default=False)
     employer = ForeignKeyField(Employer, backref="jobs")
-    skills = ManyToManyField(Skill, backref="jobs")
-    # requests
 
-    @property
-    def requirements_list(self) -> list[str]:
-        return str(self.requirements).split(";")
+    # requests < JobRequests.job
 
     @property
     def salary(self) -> dict[str:int] | None:
@@ -200,7 +303,7 @@ class Job(BaseModel):
         if delta.days == 1:
             unit = "دیروز"
         elif 1 < delta.days < 7:
-            unit = "روز قبل"
+            unit = "روز پیش"
             amount = delta.days
         elif 1 <= delta.days < 30:
             unit = "هفته قبل"
@@ -211,9 +314,7 @@ class Job(BaseModel):
         return dict(unit=unit, amount=amount)
 
 
-JobSkill = Job.skills.get_through_model()
-
-
+@add_table
 class JobRequest(BaseModel):
     job = ForeignKeyField(Job, backref="requests")
     seeker = ForeignKeyField(Seeker, backref="job_requests")
@@ -235,9 +336,12 @@ TABLES = (
     Question,
     Answer,
     Skill,
-    SkillSeeker,
+    SeekerSkill,
     JobSkill,
     Seeker,
     Employer,
     Job,
+    Personality.users.get_through_model(),
+    Personality.job_cats.get_through_model(),
+    Personality.guides.get_through_model(),
 )
