6fb2504535b52b692434417f7df2ae5caf5b175e
bsimjoo <bsimjoo@gmail.com>
2023-06-24 17:39:51 +0330

    rename `users` -> `me`

--- app/routers/me.py
+++ app/routers/me.py
@@ -8,6 +8,7 @@ from app.models.schemas import (
     LoginInfo,
     LoginResult,
     SignupInfo,
+    PersonalitySchema,
 )
 from app.models.dbmodel import Seeker, Employer, User, Role
 from peewee import IntegrityError
@@ -18,8 +19,19 @@ from typing import Annotated
 router = APIRouter()
 
 
-@router.get("/me")
+@router.get("/")
 async def get_me(
     current_user: Annotated[User, Security(get_current_user, scopes=Scopes.me)]
 ) -> UserSchema:
     return current_user.to_schema(UserSchema)
+
+
+@router.get("/personlities")
+async def get_personlities(
+    current_user: Annotated[
+        User, Security(get_current_user, scopes=Scopes.me + Scopes.seeker)
+    ]
+) -> list[PersonalitySchema]:
+    return list(
+        map(lambda x: x.to_schema(PersonalitySchema), current_user.seeker.select())
+    )
